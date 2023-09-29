b338a309720580adf2b3e406984335747455755c
mitchellshera <mitchellwachera@gmail.com>
2023-09-29 13:10:21 +0300

    task 3 and 4 api files

---
+++ api/v1/views/index.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify
+from models import storage
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
