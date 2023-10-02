0cab1196617f81d0044d422e6765d0037e73b539
mitchellshera <mitchellwachera@gmail.com>
2023-10-02 14:37:04 +0300

    CITIES

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,14 +5,13 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request, Flask
 from models import storage
 from models.city import City
-from models.state import State
 
 app = Flask(__name__)
 
 @app_views.route('/states/<string:state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities_by_state(state_id):
     """Retrieves the list of all City objects of a State"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
 
@@ -23,7 +22,7 @@ def get_cities_by_state(state_id):
 @app_views.route('/cities/<string:city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """ Retrieves a specific City """
-    city = storage.get(City, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
@@ -32,7 +31,7 @@ def get_city(city_id):
 @app_views.route('/cities/<string:city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City Object"""
-    city = storage.get(City, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
     city.delete()
@@ -43,7 +42,7 @@ def delete_city(city_id):
 @app_views.route('/states/<string:state_id>/cities', methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """Creates a new City"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
 
@@ -54,7 +53,7 @@ def post_city(state_id):
         return make_response(jsonify({'error': 'Missing name'}), 400)
 
     data = request.get_json()
-    data['state_id'] = state_id  # Set the state_id based on the route parameter
+    data['state_id'] = state_id
     city = City(**data)
     city.save()
     return make_response(jsonify(city.to_dict()), 201)
@@ -63,7 +62,7 @@ def post_city(state_id):
 @app_views.route('/cities/<string:city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
     """Updates a City"""
-    city = storage.get(City, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
 
