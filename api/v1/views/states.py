0029fe9aac956fae13bf3cccd6cae3b2a4ab30d3
mitchellshera <mitchellwachera@gmail.com>
2023-10-02 13:13:35 +0300

    states

---
+++ api/v1/views/states.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+""" These objects handle all the default RESTful API actions for States """
+from models.state import State
+from models import storage
+from flask import abort, jsonify, make_response, request, Flask
+from flasgger import Swagger
+
+app = Flask(__name__)
+Swagger(app)
+
+@app.route('/api/v1/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = [state.to_dict() for state in all_states]
+    return jsonify(list_states)
+
+@app.route('/api/v1/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
+
+@app.route('/api/v1/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+@app.route('/api/v1/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """
+    Creates a State
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = State(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+@app.route('/api/v1/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """
+    Updates a State
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
