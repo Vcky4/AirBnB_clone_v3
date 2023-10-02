45b36513844f58ab9b5ec98a1c6260d7daceae5e
Vcky4 <okon.victor.u@gmail.com>
2023-10-02 20:49:11 +0100

    feet: add not found resonse

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,10 @@ def teardown(error):
     """closes storage """
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     """main function """
