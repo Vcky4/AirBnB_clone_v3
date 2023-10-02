70aa4f8cf8679bbb01ac02664ad54e19ab9ce2db
mitchellshera <mitchellwachera@gmail.com>
2023-10-02 12:11:05 +0300

    corr

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,22 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+from os import environ
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(error):
+    """closes storage """
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,21 +2,26 @@
 """ indexapi """
 from flask import Blueprint, jsonify
 from models import storage
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-    return jsonify(stats)
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objects = {}
+    for i in range(len(classes)):
+        num_objects[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objects)
