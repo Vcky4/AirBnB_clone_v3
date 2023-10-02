d58de3a2e0e411cf3a27dab8cf96d599c0695af6
Vcky4 <okon.victor.u@gmail.com>
2023-10-02 20:53:32 +0100

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-""" Flask app for hbnb """
-from flask import Flask
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(error):
-    """closes storage """
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
 if __name__ == "__main__":
-    """main function """
-    host = environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
